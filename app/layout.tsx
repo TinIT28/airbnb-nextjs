e834783771912a9cc2edc84f981da6d59d6309db
Tin Nguyen <tinitgithub@gmail.com>
2023-07-23 22:27:46 +0700

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,35 @@
-import './globals.css'
-import { Nunito } from 'next/font/google';
-import Navbar from './components/navbar/Navbar';
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 const font = Nunito({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
-  )
+  );
 }
